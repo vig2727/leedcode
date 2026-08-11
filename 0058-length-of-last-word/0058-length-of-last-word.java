55a6e82601af1f9beaed54780dc1fe7bc34fd286
vig2727 <717824i362@kce.ac.in>
2026-08-11 11:41:00 +0530

    Time: 0 ms (100%), Space: 43 MB (52.34%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,10 +1,15 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
+        s=s.trim();
+        int i = s.length()-1;
+        int l =0;
 
-        int l = s.lastIndexOf(' ');
-
-        return s.length()-l-1;
+        while(i>=0 && s.charAt(i)!=' '){
+            i=i-1;
+            l=l+1;
+        }
+        
+        return l;
         
     }
 }
